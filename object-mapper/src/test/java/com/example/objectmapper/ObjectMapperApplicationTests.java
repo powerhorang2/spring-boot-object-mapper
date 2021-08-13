0b6816e8a1ba5d21f780079fcb0ae542957c5ed4
powerhorang2 <tjrdbqls3@naver.com>
2021-08-13 17:20:16 +0900

    Object Mapper / 프로젝트 생성

---
+++ object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.objectmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+class ObjectMapperApplicationTests {
+	
+	@Test
+	void contextLoads(){
+	}
+
+}
